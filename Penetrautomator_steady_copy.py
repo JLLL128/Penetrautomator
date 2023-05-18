f51db35f764b9852e60ab2d088a938c45849fc4a
JLLL128 <1913983011@qq.com>
2023-05-18 13:05:17 +0800

    “change_arg_test_JL”

--- Penetrautomator_steady_copy.py
+++ Penetrautomator_steady_copy.py
@@ -8,7 +8,6 @@ from requests.exceptions import MissingSchema
 from urllib.parse import urlparse
 import datetime
 import os
-import argparse  # 引入argparse库
 
 TARGET_URL = None
 TIMESTAMP = datetime.datetime.now().strftime('%Y%m%d%H%M%S')  # 添加全局变量 TIME
@@ -25,17 +24,8 @@ def get_url_with_scheme(url):
 
 def main():
     global TARGET_URL
-
-    # 创建argparse对象
-    parser = argparse.ArgumentParser(description="网站安全检测工具")
-    parser.add_argument("-u", "--url", required=True, help="TARGET_URL")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-a","--all",action="store_true",help="full scan")
-    group.add_argument("-c","--cms",action="store_true",help="cms identification only")
-    args = parser.parse_args()
-
     try:
-        input_url = args.url
+        input_url = input("请输入目标网站url：")
         TARGET_URL = get_url_with_scheme(input_url)
     except MissingSchema:
         print("无效的URL，请确保输入正确的网址，例如www.example.com")
@@ -45,22 +35,18 @@ def main():
     output_dir = os.path.join('output_file', f'{TIMESTAMP}')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+    #print(urlparse(TARGET_URL).netloc)
+    # Running CMS_main
+    CMS_main.get_CMS(TARGET_URL,TIMESTAMP)
 
-    if args.cms:
-        CMS_main.get_CMS(TARGET_URL, TIMESTAMP)
+    # Running Directory_main
+    Directory_main.get_directory(TARGET_URL,TIMESTAMP)
 
-    else:
-        # Running CMS_main
-        CMS_main.get_CMS(TARGET_URL,TIMESTAMP)
+    # Running subDomains_main
+    subDomains_main.get_subdomains(TARGET_URL,TIMESTAMP)
 
-        # Running Directory_main
-        Directory_main.get_directory(TARGET_URL,TIMESTAMP)
-
-        # Running subDomains_main
-        subDomains_main.get_subdomains(TARGET_URL,TIMESTAMP)
-
-        # Running port_main
-        port_main.get_port(TARGET_URL,TIMESTAMP)
+    # Running port_main
+    port_main.get_port(TARGET_URL,TIMESTAMP)
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Penetrautomator
 An automated penetration testing tool
+
+## 目前实现功能
+
+- CMS查询（调用whatcms的api）
+- 路径爆破（调用dirsearch）
+- 端口扫描（调用nmap的命令）
+- 子域名爆破（调用subDomainBrute）
+
+## 用法
+
+Penetrautomator.py是命令行模式
+
+```
+#在终端里面输入
+python Penetrautomator.py -u target_url --all //完整扫描
+python Penetrautomator.py -u target_url --cms //只查询cms
+#-u是必加参数，后面的参数不带默认完整扫描
+```
+
+![image-20230518125803575](https://jiangxiaoyyds.com/img/Penetrautomator/image-20230518125803575.png)
+
+![image-20230518125845146](https://jiangxiaoyyds.com/img/Penetrautomator/image-20230518125845146.png)
+
+Penetrautomator_steady_copy.py是稳定的脚本，没有命令行，直接运行就好
+
+## 
